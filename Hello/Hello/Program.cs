2289e321f8798b38009b35a1b82b1dc2cf1c9068
JudaYeung <95304844+JudaYeung@users.noreply.github.com>
2022-05-06 18:02:15 +0300

    Add files via upload

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = ReadArray();
+
+            int target = ReadTarget();
+
+            int FindIndex = BinarySearch(array,target);
+
+            Console.WriteLine("Index element = " + FindIndex);
+
+        }
+
+        private int BinarySearch(int[] array, int target)
+        {
+            int l = 0;
+            int r = array.Length - 1;
+            while (l <= r)
+            {
+                int mid = l + (r - 1) / 2;
+                if (target < array[mid])
+                    r = mid - 1;
+                else if (target > array[mid])
+                    l = mid + 1;
+                else
+                    return mid;
+            
+            }
+            return -1;
+
+        }
+
+        private static int[] ReadArray()
+        {
+            Console.WriteLine("Enter number of elements:");
+            string line = Console.ReadLine();
+            int count = Int32.Parse(line);
+
+            int[] array = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Enter element " + i);
+                string current = Console.ReadLine();
+                array[i] = Int32.Parse(current);
+            }
+
+            return array;
+
+        }
+
+        private static int ReadTarget()
+        {
+            Console.WriteLine("Enter target number:");
+            string line = Console.ReadLine();
+            int target = Int32.Parse(line);
+
+            return target;
+        }
+    }
+
+}
